673983aca06fa0b39626d9b27b42ffbf59fc717c
zaraazzar <azzar.arza18@gmail.com>
2023-09-26 19:45:44 +0700

    Action Widget

--- MyWidgets/app/src/main/java/com/dicoding/mywidgets/NumberGenerator.kt
+++ MyWidgets/app/src/main/java/com/dicoding/mywidgets/NumberGenerator.kt
@@ -3,6 +3,7 @@ package com.dicoding.mywidgets
 import java.util.*
 
 internal object NumberGenerator {
+    //helper class
     fun generate(max: Int): Int {
         val random = Random()
         return random.nextInt(max)
