6a7ff52d39f79cd20bb7aba25d8b2138005eae7e
zaraazzar <azzar.arza18@gmail.com>
2023-09-27 15:35:33 +0700

    WebView

---
+++ MyWebView/app/src/main/java/com/dicoding/mywebview/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.dicoding.mywebview
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: JsResult
+            ): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                result.confirm()
+                return true
+            }
+        }
+
+        webView.loadUrl("https://www.dicoding.com")
+
+    }
+}
