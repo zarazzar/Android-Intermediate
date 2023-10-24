c9b1b1a40c307594da10029cf83cc53fcaea0356
zaraazzar <azzar.arza18@gmail.com>
2023-10-24 21:30:49 +0700

    RemoteMediator

---
+++ LatihanPaging3 (starter)/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
@@ -0,0 +1,28 @@
+package com.dicoding.myunlimitedquotes.data
+
+import androidx.lifecycle.LiveData
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.liveData
+import com.dicoding.myunlimitedquotes.database.QuoteDatabase
+import com.dicoding.myunlimitedquotes.network.ApiService
+import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
+
+class QuoteRepository(private val quoteDatabase: QuoteDatabase, private val apiService: ApiService) {
+    fun getQuote(): LiveData<PagingData<QuoteResponseItem>> {
+        @OptIn(ExperimentalPagingApi::class)
+        return Pager(
+            config = PagingConfig(
+                pageSize = 5
+            ),
+            remoteMediator = QuoteRemoteMediator(quoteDatabase,apiService),
+            pagingSourceFactory = {
+//                QuotePagingSource(apiService)
+                quoteDatabase.quoteDao().getAllQuote()
+            }
+        ).liveData
+
+    }
+}
